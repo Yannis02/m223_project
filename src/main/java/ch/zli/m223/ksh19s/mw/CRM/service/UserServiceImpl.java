984f0c45a3ff708351181724524cd9b941a55b45
yanni <yanni@DESKTOP-OI9P5KP>
2022-07-13 18:20:33 +0200

    initial commit

--- src/main/java/ch/zli/m223/ksh19s/mw/CRM/service/UserServiceImpl.java
+++ src/main/java/ch/zli/m223/ksh19s/mw/CRM/service/UserServiceImpl.java
@@ -17,7 +17,7 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Override
 	public List<AppUser> getAllUsers() {
 		return new ArrayList<>(userRepository.findAll());
@@ -31,19 +31,23 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public AppUser insertUser(String name) {
-		if (name == null) throw new InvalidArgumentException("Name must not be null");
+	public AppUser insertUser(String name, String password) {
+		if (name == null)
+			throw new InvalidArgumentException("Name must not be null");
 		// If (user with userName exists) then throw UserAllreadyExistsException
 		if (userRepository.findUserByName(name).isPresent()) {
 			throw new UserAllreadyExistsException("User with name" + name + " already exists");
 		}
-		return userRepository.insert(name);
+		return userRepository.insert(name, password);
 	}
 
 	@Override
 	public void deleteUserById(Long id) {
-		if (id == null) throw new InvalidArgumentException("Id must not be null");
-		if (userRepository.findById(id).isEmpty()) { return; }
+		if (id == null)
+			throw new InvalidArgumentException("Id must not be null");
+		if (userRepository.findById(id).isEmpty()) {
+			return;
+		}
 		userRepository.deleteById(id);
 	}
 
